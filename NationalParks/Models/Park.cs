e97d903a698e743e669ae1c2d3a45a436d8228a1
bodwood <bodiewood@hotmail.com>
2023-01-20 08:46:45 -0800

    add park class with properties for sql db

---
+++ NationalParks/Models/Park.cs
@@ -0,0 +1,10 @@
+namespace NationalParks.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string ParkName { get; set; }
+    public string Region { get; set; }
+    public string State { get; set; }
+  }
+}
