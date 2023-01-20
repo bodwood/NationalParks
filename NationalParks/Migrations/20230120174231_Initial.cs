f48a808c65bfe6dd0252ecf3c0465f42cd4b8d84
bodwood <bodiewood@hotmail.com>
2023-01-20 09:44:06 -0800

    add migrations data

---
+++ NationalParks/Migrations/20230120174231_Initial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NationalParks.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ParkName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Region = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    StateName = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "ParkName", "Rating", "Region", "StateName" },
+                values: new object[,]
+                {
+                    { 1, "Zion", 5, "Mountain", "Utah" },
+                    { 2, "Bryce Canyon", 4, "Mountain", "Utah" },
+                    { 3, "Crater Lake", 4, "Pacific West", "Oregon" },
+                    { 4, "Mount Rainier", 4, "Pacific West", "Washington" },
+                    { 5, "Denali", 5, "Pacific West", "Alaska" },
+                    { 6, "Acadia", 3, "Northeast", "Maine" },
+                    { 7, "Badlands", 2, "Midwest", "South Dakota" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
