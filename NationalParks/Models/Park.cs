0718d4ca319aa62a49ad1a934d75e2b9074cfde0
bodwood <bodiewood@hotmail.com>
2023-01-20 09:28:25 -0800

    make fields required

--- NationalParks/Models/Park.cs
+++ NationalParks/Models/Park.cs
@@ -1,11 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NationalParks.Models
 {
   public class Park
   {
     public int ParkId { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string ParkName { get; set; }
+
+    [Required]
+    [StringLength(30)]
     public string Region { get; set; }
+
+    [Required]
+    [StringLength(20)]
     public string State { get; set; }
+
+    [Required]
+    [Range(0, 200, ErrorMessage = "Rating must be between 0 and 5.")]
     public int Rating { get; set; }
   }
 }
