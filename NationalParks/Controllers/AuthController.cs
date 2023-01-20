d89edc45301395cdeeb1beea7d5ad85bff46b66b
bodwood <bodiewood@hotmail.com>
2023-01-20 13:40:34 -0800

    add method creating jwt token after a user registers and logs in

--- NationalParks/Controllers/AuthController.cs
+++ NationalParks/Controllers/AuthController.cs
@@ -14,14 +14,14 @@ namespace NationalParks.Controllers
   public class AuthController : ControllerBase
   {
     public static User user = new User();
-    // private readonly IConfiguration _configuration;
+    private readonly IConfiguration _configuration;
     // private readonly IUserService _userService;
 
-    //  public AuthController(IConfiguration configuration, IUserService userService)
-    // {
-    //   _configuration = configuration;
-    //   _userService = userService;
-    // }
+    public AuthController(IConfiguration configuration/* IUserService userService*/)
+    {
+      _configuration = configuration;
+      // _userService = userService;
+    }
 
     [HttpPost("register")]
     public async Task<ActionResult<User>> Register(UserDto request)
@@ -38,11 +38,11 @@ namespace NationalParks.Controllers
     [HttpPost("login")]
     public async Task<ActionResult<string>> Login(UserDto request)
     {
-      if(user.Username != request.Username)
+      if (user.Username != request.Username)
       {
         return BadRequest("User not found.");
       }
-      if(!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+      if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
       {
         return BadRequest("Wrong password.");
       }
@@ -55,15 +55,27 @@ namespace NationalParks.Controllers
     {
       List<Claim> claims = new List<Claim>
       {
-        new 
-      }
+        new Claim(ClaimTypes.Name, user.Username)
+      };
 
-      return string.Empty;
+      var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+        _configuration.GetSection("AppSettings:Token").Value));
+
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+      var token = new JwtSecurityToken(
+        claims: claims,
+        expires: DateTime.Now.AddDays(1),
+        signingCredentials: creds);
+
+      var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+      return jwt;
     }
 
     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-      using(var hmac = new HMACSHA512())
+      using (var hmac = new HMACSHA512())
       {
         passwordSalt = hmac.Key;
         passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
@@ -72,7 +84,7 @@ namespace NationalParks.Controllers
 
     private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
     {
-      using(var hmac = new HMACSHA512(passwordSalt))
+      using (var hmac = new HMACSHA512(passwordSalt))
       {
         var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
         return computedHash.SequenceEqual(passwordHash);
