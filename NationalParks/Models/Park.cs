1621f807f8deee07c1e8bca87997c74a30cd18ab
bodwood <bodiewood@hotmail.com>
2023-01-20 16:43:29 -0800

    update required fields

--- NationalParks/Models/Park.cs
+++ NationalParks/Models/Park.cs
@@ -5,21 +5,9 @@ namespace NationalParks.Models
   public class Park
   {
     public int ParkId { get; set; }
-
-    [Required]
-    [StringLength(50)]
     public string ParkName { get; set; }
-
-    [Required]
-    [StringLength(30)]
     public string Region { get; set; }
-
-    [Required]
-    [StringLength(20)]
     public string StateName { get; set; }
-
-    [Required]
-    [Range(0, 200, ErrorMessage = "Rating must be between 0 and 5.")]
     public int Rating { get; set; }
   }
 }
